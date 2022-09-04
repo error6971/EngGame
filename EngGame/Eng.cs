82ac0f70d9adeb1a257548aa73a49e6b4b34afef
Hossein <hossinsalhi73@google.com>
2022-09-04 18:31:51 +0430

    Add project files.

---
+++ EngGame/Eng.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace EngGame
+{
+    internal class Eng
+    {
+        private void Start()
+        {
+            confingGame();
+            ConfingPlayers();
+            Setup();
+            StartGame();
+        }
+
+        private void confingGame(int PlayerCount = 4, Player[] Players = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ConfingPlayers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EngGame/Player.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace EngGame
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public int Name { get; set; }
+        public int Token { get; set; }
+    }
+}
